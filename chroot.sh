0286b5518b6ba2f9dc6d4173d3792ccf5af59968
edrianjansolatorio <edrian.solatorio@arbcalls.com>
2022-06-05 01:25:39 +0800

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -15,7 +15,7 @@ locale > /etc/locale.conf
 
 hwclock --systohc --utc
 
-checkline "echo '\$USERNAME: ${USERNAME}'"
+checkline "echo '\$USERNAME: $USERNAME'"
 
 echo "root:${PASSWORD}" | chpasswd
 useradd -m $USERNAME
