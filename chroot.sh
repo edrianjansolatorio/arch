80ea885cddda3e57e5c1c9c3f0645700536aeefc
edrianjansolatorio <edrian.solatorio@arbcalls.com>
2022-06-04 15:18:14 +0800

    Update STEPS.md, aur.txt, and 3 more files...

--- chroot.sh
+++ chroot.sh
@@ -1,58 +1,63 @@
 #!/bin/bash
 
-debug () {
+checkline() {
 $1
-read -p "debugging"
+read -p "continue?" confirmation
+if [[ "$confirmation" != "y" ]]; then
+exit 0
+fi
 }
 
 source ./settings.conf
 
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+ln -sf /mnt/usr/share/zoneinfo/Asia/Manila /etc/localtime
 locale-gen
 hwclock --systohc --utc
 
-passwd << EOF
-$PASSWORD
-$PASSWORD
-EOF
-
+echo -n "${PASSWORD}" | passwd root -
 useradd -m $USERNAME
-passwd scrubs << EOF
-$PASSWORD
-$PASSWORD
-EOF
+echo -n "${PASSWORD}" | passwd $USERNAME -
 
 whereis sudo
 usermod -aG wheel,audio,video,optical,storage $USERNAME
 groups $USERNAME
 
 # ---------------- DEBUG AREA ------------------ #
-./apps.sh
+
+# @@@ #
+# ./apps.sh
+# @@@ #
 
 # ---------------- DEBUG AREA ------------------ #
 
 # ---------- G14 kernel ---------- #
 
-echo "[Unit]
-Description=Set the battery charge threshold
-After=multi-user.target
-StartLimitBurst=0
+# @@@ #
 
-[Service]
-Type=oneshot
-Restart=on-failure
-ExecStart=/bin/bash -c 'echo 60 > /sys/class/power_supply/BAT1/charge_control_end_threshold'
+# echo "[Unit]
+# Description=Set the battery charge threshold
+# After=multi-user.target
+# StartLimitBurst=0
 
-[Install]
-WantedBy=multi-user.target" >> /etc/systemd/system/battery-charge-threshold.service
+# [Service]
+# Type=oneshot
+# Restart=on-failure
+# ExecStart=/bin/bash -c 'echo 60 > /sys/class/power_supply/BAT1/charge_control_end_threshold'
 
-pacman -Syu
+# [Install]
+# WantedBy=multi-user.target" >> /etc/systemd/system/battery-charge-threshold.service
 
-pacman -Sy --noconfirm --needed asusctl supergfxctl linux-g14 linux-g14-headers
+# pacman -Syu
 
-systemctl enable supergfxd
-systemctl enable power-profiles-daemon.service
-systemctl --user enable asus-notify.service
-systemctl enable battery-charge-threshold.service
+# pacman -Sy --noconfirm --needed asusctl supergfxctl linux-g14 linux-g14-headers
+
+# systemctl enable supergfxd
+# systemctl enable power-profiles-daemon.service
+# systemctl --user enable asus-notify.service
+# systemctl enable battery-charge-threshold.service
+
+# @@@ #
 
 # ---------- G14 kernel ---------- #
 
@@ -69,6 +74,42 @@ fi
 systemctl enable bluetooth
 
 # ---------- RUN SERVICES ------------ #
+
+# ---------- ENCRYPT SET-UP ---------- #
+# "shingha" <--- custom name
+
+STORAGE_NAME=shingha
+VOLGROUP=scrubs
+
+sed 's/HOOKS="base udev autodetect modconf block filesystems keyboard fsck"/HOOKS="base udev autodetect modconf block filesystems keyboard fsck encrypt lvm2"/g' /etc/mkinitpcio.conf
+
+checkline "cat /etc/mkinitcpio.conf | less"
+
+mkinitcpio -p linux
+bootctl --path=/boot/ install
+
+checkline
+
+echo "
+default arch
+timeout 3
+editor 0
+" > /etc/loader/loader.conf
+
+checkline "cat /etc/loader/loader.conf | less"
+
+# blkid /dev/${DISK}${DISK_PREFIX}2
+
+# echo "
+# title Shingha System
+# linux /vmlinuz-linux
+# initrd /initramfs-linux.img
+# options cryptdevice=UUID=
+# "
+
+exit 0
+
+# ---------- ENCRYPT SET-UP ---------- #
 
 if [ "$BOOT_TYPE" == "EFI" ]; then
     mkdir /boot/efi
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 checkline() {
 $1
 read -p "continue?" confirmation
-if [[ "$confirmation" == "n" ]]; then
+if [[ "$confirmation" != "y" ]]; then
 exit 0
 fi
 }
@@ -24,23 +24,43 @@ sgdisk -Z ${DISK}
 sgdisk -a 2048 -o ${DISK}
 
 sgdisk -n 1::+512M --typecode=1:ef00 --change-name=1:'EFI' ${DISK} # EFI /dev/nvme0n1p1
-sgdisk -n 2::+4g --typecode=2:8200 --change-name=2:'SWAP' ${DISK} # SWAP /dev/nvme0n1p2
-sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # FILE SYSTEM /dev/nvme0n1p3
+sgdisk -n 2::-0 --typecode=2:8e00 --change-name=2:'ROOT' ${DISK} # FILE SYSTEM /dev/nvme0n1p3
 
 NEW_DISK=${DISK}${DISK_PREFIX}
 
-# ------------- DEBUG: ENCRYPT SET-UP ------------------- #
-echo -n "${PASSWORD}" | cryptsetup -y -v luksFormat ${NEW_DISK}3 -
-echo -n "${PASSWORD}" | cryptsetup open ${NEW_DISK}3 ROOT -
-mkfs.btrfs -L ROOT ${NEW_DISK}3
-mount -t btrfs ${NEW_DISK}3 /mnt
-# ------------- DEBUG: ENCRYPT SET-UP ------------------- #
-# echo "Y" | mkfs.ext4 ${NEW_DISK}3
-
 mkfs.fat -F32 ${NEW_DISK}1
-mkswap ${NEW_DISK}2
-swapon ${NEW_DISK}2
-# mount ${NEW_DISK}3 /mnt
+
+# ------------- DEBUG: ENCRYPT SET-UP ------------------- #
+
+# "shingha" <--- custom name
+
+STORAGE_NAME=shingha
+VOLGROUP=scrubs
+
+cryptsetup luksFormat /dev/sda2
+cryptsetup open --type luks /dev/sda2 ${STORAGE_NAME}
+
+if [[ ! "/dev/mapper/${STORAGE_NAME}" ]]; then
+	exit 0
+fi
+
+pvcreate /dev/mapper/${STORAGE_NAME}
+vgcreate ${VOLGROUP} /dev/mapper/${STORAGE_NAME}
+lvcreate -L2G ${VOLGROUP} -n SWAP
+lvcreate -l 100%FREE ${VOLGROUP} -n ROOT
+
+mkfs.ext4 /dev/mapper/${VOLGROUP}-ROOT
+mkswap /dev/mapper/${VOLGROUP}-SWAP
+mount /dev/mapper/${VOLGROUP}-ROOT /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+swapon /dev/mapper/${VOLGROUP}-SWAP
+
+lsblk
+fdisk -l
+
+# ------------- DEBUG: ENCRYPT SET-UP ------------------- #
+
 
 elif [ "$BOOT_TYPE" == "LEGACY" ]; then
 
@@ -54,6 +74,8 @@ sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BOOT' ${DISK} # BOOT /dev/nv
 sgdisk -n 2::+2g --typecode=2:8200 --change-name=2:'SWAP' ${DISK} # SWAP /dev/nvme0n1p2
 sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # FILE SYSTEM /dev/nvme0n1p3
 
+sgdisk -A 1:set:2 ${DISK}
+
 NEW_DISK=${DISK}${DISK_PREFIX}
 
 echo "Y" | mkfs.ext2 ${NEW_DISK}1
@@ -63,11 +85,13 @@ mkswap ${NEW_DISK}2
 mount ${NEW_DISK}3 /mnt
 fi
 
-lsblk
 fdisk -l
+checkline "lsblk"
 
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-reflector --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+# @@@ #
+# cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+# reflector --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+# @@@ #
 
 # -- debugging -- #
 
@@ -83,29 +107,31 @@ cat /mnt/etc/fstab
 
 # ----------------- KDE ------------------ #
 
-pacstrap -i /mnt --needed --noconfirm networkmanager dhcpcd dhclient netctl dialog iwd
+# @@@ #
+# pacstrap -i /mnt --needed --noconfirm networkmanager dhcpcd dhclient netctl dialog iwd
+# @@@ #
 
 if [ "$DESKTOP" == "KDE" ]; then
 
 ### OPTION-1
-pacstrap -i /mnt --needed --noconfirm xorg plasma-desktop plasma-wayland-session sddm
+
+# @@@ #
+# pacstrap -i /mnt --needed --noconfirm xorg plasma-desktop plasma-wayland-session sddm
+# @@@ #
 
 fi
 
 # ---------------- GPU ------------------- #
 
 if [ "$GPU_TYPE" == "NVIDIA" ]; then
-read -p "debug: nvidia"
-pacstrap -i /mnt --needed --noconfirm cuda lib32-libvdpau lib32-nvidia-utils lib32-opencl-nvidia libvdpau libxnvctrl nvidia-settings nvidia-utils opencl-nvidia nvidia-dkms
-read -p "debug: nvidia"
+# @@@ #
+# pacstrap -i /mnt --needed --noconfirm cuda lib32-libvdpau lib32-nvidia-utils lib32-opencl-nvidia libvdpau libxnvctrl nvidia-settings nvidia-utils opencl-nvidia nvidia-dkms
+# @@@ #
 fi
 
 # ---------------- GPU ------------------- #
 
 cp /etc/pacman.d/mirrorlist.bak /etc/pacman.d/mirrorlist
-
-sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /mnt/etc/locale.gen
-ln -sf /mnt/usr/share/zoneinfo/Asia/Manila /mnt/etc/localtime
 
 echo $HOST_NAME >> /mnt/etc/hostname
 echo "127.0.0.1 localhost" >> /mnt/etc/hosts
