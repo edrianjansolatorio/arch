8e486ac90e8e0285f253d6a999b0b9ee7abee1ea
edrianjansolatorio <edrian.solatorio@arbcalls.com>
2022-06-04 22:59:49 +0800

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -15,9 +15,9 @@ locale > /etc/locale.conf
 
 hwclock --systohc --utc
 
-echo -n "root:${PASSWORD}" | chpasswd
+echo "root:${PASSWORD}" | chpasswd
 useradd -m $USERNAME
-echo -n "$USERNAME:${PASSWORD}" | chpasswd
+echo "$USERNAME:${PASSWORD}" | chpasswd
 
 whereis sudo
 usermod -aG wheel,audio,video,optical,storage $USERNAME
